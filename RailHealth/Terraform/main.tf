0fa33dcb75066fc38db78632b9d185b648149f54
Mihaly Kacso <mihaly.kasco@partner.bmw.de>
2024-03-14 19:03:12 +0200

    eventhub capture

--- RailHealth/Terraform/main.tf
+++ RailHealth/Terraform/main.tf
@@ -47,36 +47,35 @@ resource "azurerm_storage_container" "containerTFState" {
   storage_account_name  = azurerm_storage_account.storageRailHealth.name
   container_access_type = "private"
 }
-
-resource "azurerm_kusto_cluster" "kcrailhealth" {
-  name                = "kcrailhealth"
-  location            = "West Europe"
-  resource_group_name = azurerm_resource_group.rgRailHealth.name
-  sku {
-    name     = "Dev(No SLA)_Standard_E2a_v4"
-    capacity = 1
-  }
-
-  zones = ["1", "3", "2"]
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    PROJECT = local.tag
-  }
-
-  trusted_external_tenants = []
-  language_extensions      = []
-
+resource "azurerm_storage_container" "containerMearurements" {
+  name                  = "measurements"
+  storage_account_name  = azurerm_storage_account.storageRailHealth.name
+  container_access_type = "private"
 }
 
-variable "storageAccounts_railmeasurements_externalid" {
-  type    = string
-  default = "/subscriptions/c0c15b2e-abda-4c06-9e00-9bf67a616503/resourceGroups/Rail-Health/providers/Microsoft.Storage/storageAccounts/storagerailhealth"
-}
+# resource "azurerm_kusto_cluster" "kcrailhealth" {
+#   name                = "kcrailhealth"
+#   location            = "West Europe"
+#   resource_group_name = azurerm_resource_group.rgRailHealth.name
+#   sku {
+#     name     = "Dev(No SLA)_Standard_E2a_v4"
+#     capacity = 1
+#   }
 
+#   zones = ["1", "3", "2"]
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+
+#   tags = {
+#     PROJECT = local.tag
+#   }
+
+#   trusted_external_tenants = []
+#   language_extensions      = []
+
+# }
 resource "azurerm_eventhub" "eventHubRailHealth" {
     name = "eventHubRailHealth"
     namespace_name = azurerm_eventhub_namespace.rail_health_event_hub_ns.name
@@ -92,11 +91,12 @@ resource "azurerm_eventhub" "eventHubRailHealth" {
         name = "EventHubArchive.AzureBlockBlob"
         archive_name_format = "{Namespace}/{EventHub}/{PartitionId}/{Year}/{Month}/{Day}/{Hour}/{Minute}/{Second}"
         blob_container_name = "measurements"
-        storage_account_id = var.storageAccounts_railmeasurements_externalid
+        storage_account_id = azurerm_storage_account.storageRailHealth.id
       }
       interval_in_seconds = 300
       size_limit_in_bytes = 314572800
     }
+    depends_on = [azurerm_role_assignment.eventhub_storage_owner]
 }
 
 resource "azurerm_eventhub_namespace" "rail_health_event_hub_ns" {
@@ -120,26 +120,12 @@ resource "azurerm_eventhub_namespace" "rail_health_event_hub_ns" {
   }
 }
 
-resource "azurerm_role_assignment" "eventhub_storage_Owner" {
+resource "azurerm_role_assignment" "eventhub_storage_owner" {
   scope                = azurerm_storage_account.storageRailHealth.id
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = azurerm_eventhub_namespace.rail_health_event_hub_ns.identity[0].principal_id
-
-  depends_on = [azurerm_eventhub_namespace.rail_health_event_hub_ns]
 }
 
-
-resource "azurerm_eventhub_namespace_authorization_rule" "rail_health_event_hub_ns_auth_rule" {
-    name                = "RootManageSharedAccessKey"
-    namespace_name      = azurerm_eventhub_namespace.rail_health_event_hub_ns.name
-    resource_group_name = azurerm_resource_group.rgRailHealth.name
-
-    depends_on = [azurerm_eventhub_namespace.rail_health_event_hub_ns]
-
-    listen = true
-    manage = true
-    send = true
-}
 
 resource "azurerm_eventhub_consumer_group" "rail_telemetry_default_consumer_group" {
     name                = "Default"
@@ -164,21 +150,3 @@ resource "azurerm_network_security_group" "rail_health_event_hub_ns_nsg" {
       PROJECT = local.tag
     } 
 }
-
-resource "azurerm_network_security_rule" "rail_health_event_hub_ns_nsg_rule" {
-    name                        = "eventHubNamespaceRailHealthNSGRule"
-    priority                    = 100
-    direction                   = "Inbound"
-    access                      = "Allow"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = 443
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*" 
-    resource_group_name         = azurerm_resource_group.rgRailHealth.name
-    network_security_group_name = azurerm_network_security_group.rail_health_event_hub_ns_nsg.name
-}
-
-
-
-
